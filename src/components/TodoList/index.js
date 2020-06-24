8066b741f7694b0419b1ee3c6c8a1be06009d62d
Duy Ha <ha.le@dinovative.com>
2020-06-24 11:27:14 +0700

    intergrate reselect

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -2,18 +2,19 @@ import React, { useContext, useMemo } from 'react'
 import './style.scss';
 import TodoItem from '../TodoItem';
 import { useSelector } from 'react-redux';
+import { createSelector } from 'reselect';
 
 // import stateContext from '../../contexts/stateContext';
 // import TodoItemContainer from '../../containers/TodoItemContainer';
 
-const TodoList = () => {
-  // const todos = useContext(stateContext)
-  const filter = useSelector(state => state.filter);
-  const todos = useSelector(state => state.todos);
+const todosSelector = state => state.todos;
+const filterSelector = state => state.filter;
 
-  const todosFilter = useMemo(() => {
-    
-    switch(filter) {
+const selectFilterTodos = createSelector(
+  todosSelector,
+  filterSelector,
+  (todos, filter) => {
+    switch (filter) {
       case "complete":
         return todos.filter(todo => todo.complete);
       case "uncomplete":
@@ -21,7 +22,30 @@ const TodoList = () => {
       default:
         return todos;
     }
-  }, [filter, todos]);
+  }
+);
+
+const TodoList = () => {
+  // const todos = useContext(stateContext)
+
+
+  const todosFilter = useSelector(selectFilterTodos);
+  console.log("data ", todosFilter);
+
+  // const filter = useSelector(state => state.filter);
+  // const todos = useSelector(state => state.todos);
+
+  // const todosFilter = useMemo(() => {
+
+  //   switch (filter) {
+  //     case "complete":
+  //       return todos.filter(todo => todo.complete);
+  //     case "uncomplete":
+  //       return todos.filter(todo => !todo.complete);
+  //     default:
+  //       return todos;
+  //   }
+  // }, [filter, todos]);
   return (
     <div className="todo-list">
       {todosFilter.map(todo => <TodoItem key={todo.id} {...todo} />)}
